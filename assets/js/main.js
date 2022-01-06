da102676388ce1f8d6e010650903b32b13368e84
Talha-dev1980 <dotdev1980@gmail.com>
2022-01-06 22:11:46 +0500

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Ismail Tasdelen.", "Security Researcher.", "Bug Bounty Hunter.", "Developer."],
+      strings: ["Talha Shaukat.", "Mobile App Developer.", "Photographer."],
       typeSpeed: 100,
       loop: true,
     });
