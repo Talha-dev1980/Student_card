5e763b375900baa3cf29cba8eaee487e7d8b4650
Talha-dev1980 <dotdev1980@gmail.com>
2022-01-06 21:51:39 +0500

    first commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title>Ismail Tasdelen</title>
+  <title>Talha Shaukat</title>
   <meta content="security researcher, security consultant, bug bounty, bug bounty hunter, web security, penetration testing, cyber security, information security, siber güvenlik, bilgi güvenliği" name="keywords">
   <meta content="Security Consultant" name="description">
   <link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,500,500i,600,600i,700,700i|Playfair+Display:400,400i,700,700i,900,900i" rel="stylesheet">
@@ -18,18 +18,16 @@
 </head>
 <body>
   <div id="header" class="home">
-    <div class="container">	
+    <div class="container"> 
       <div class="header-content">
-        <h1>I'm <span class="typed"></span></h1>
-        <p>security researcher, bug bounty hunter, developer</p>
+        <h1>I Develop <span class="typed"></span></h1>
+        <p>Android Apps, Web Apps, REST APIs, much more</p>
         <ul class="list-unstyled list-social">
-		  <li><a href="https://github.com/ismailtasdelen" target="_blank"><i class="ion-social-github"></i></a></li>
-          <li><a href="https://www.linkedin.com/in/ismailtasdelen/" target="_blank"><i class="ion-social-linkedin" target="_blank"></i></a></li>
-          <li><a href="https://twitter.com/@ismailtsdln" target="_blank"><i class="ion-social-twitter"></i></a></li>
-          <li><a href="https://www.youtube.com/channel/UCPqc50Wmt6ZF2BJWtNJlq-g"><i class="ion-social-youtube" target="_blank"></i></a></li>
-		  <li><a href="https://www.instagram.com/mr.ismailtasdelen/" target="_blank"><i class="ion-social-instagram"></i></a></li>
-		  <li><a href="https://www.facebook.com/mr.ismailtasdelen" target="_blank"><i class="ion-social-facebook"></i></a></li>
-      </div>
+      <li><a href="https://github.com/Talha-dev1980" target="_blank"><i class="ion-social-github"></i></a></li>
+          <li><a href="https://www.linkedin.com/in/talha-shaukat-63993617a" target="_blank"><i class="ion-social-linkedin" target="_blank"></i></a></li>
+          <li><a href="https://www.youtube.com/BeGharzPakistan"><i class="ion-social-youtube" target="_blank"></i></a></li>
+      <li><a href="https://www.instagram.com/sheikh___talha/" target="_blank"><i class="ion-social-instagram"></i></a></li>
+          </div>
     </div>
   </div>
   <script src="assets/lib/jquery/jquery.min.js"></script>
@@ -42,3 +40,5 @@
   <script src="assets/js/main.js"></script>
 </body>
 </html>
+
+
